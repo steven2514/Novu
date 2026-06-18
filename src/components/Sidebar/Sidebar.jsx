f29258cdb454faf9f8cb71f61432b82b52e42612
Steven Alvarez <stevenalvarez699@gmail.com>
2026-06-18 02:21:28 -0500

    Paginas responsive en todo el proyecto para poder ver en movil

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,30 +1,37 @@
 import { NavLink } from "react-router-dom";
 import './Sidebar.css';
+import { useState } from 'react';
 
 function Sidebar() {
+
+    const [abierto, setAbierto] = useState(false);
+
     return (
-        <div className="sidebar">
-            <div className="sidebar-logo">
-                <div className="sidebar-logo-icono">N</div>
-                <div className="sidebar-logo-texto">
-                    <h2>NovuApp</h2>
-                    <p>Control Total</p>
+        <>
+            <button className="btn-hamburguesa" onClick={() => setAbierto(!abierto)}>☰</button>
+            <div className={`sidebar ${abierto ? 'sidebar-abierto' : ''}`}>
+                <div className="sidebar-logo">
+                    <div className="sidebar-logo-icono">N</div>
+                    <div className="sidebar-logo-texto">
+                        <h2>NovuApp</h2>
+                        <p>Control Total</p>
+                    </div>
+                </div>
+
+                <NavLink to="/" onClick={() => setAbierto(false)}>🧩 Dashboard</NavLink>
+                <NavLink to="/cuentas" onClick={() => setAbierto(false)}>🏦 Cuentas</NavLink>
+                <NavLink to="/transacciones" onClick={() => setAbierto(false)}>🔄 Transacciones</NavLink>
+                <NavLink to="/suscripciones" onClick={() => setAbierto(false)}>📅 Suscripciones</NavLink>
+                <NavLink to="/metas" onClick={() => setAbierto(false)}>🎯 Metas</NavLink>
+                <NavLink to="/calendario" onClick={() => setAbierto(false)}>📆 Calendario</NavLink>
+                <NavLink to="/aprendizaje" onClick={() => setAbierto(false)}>📚 Aprendizaje</NavLink>
+
+                <div className="sidebar-footer">
+                    <p>Autor</p>
+                    <span>Steven David Alvarez Morante</span>
                 </div>
             </div>
-
-            <NavLink to="/">🧩 Dashboard</NavLink>
-            <NavLink to="/cuentas">🏦 Cuentas</NavLink>
-            <NavLink to="/transacciones">🔄 Transacciones</NavLink>
-            <NavLink to="/suscripciones">📅 Suscripciones</NavLink>
-            <NavLink to="/metas">🎯 Metas</NavLink>
-            <NavLink to="/calendario">📆 Calendario</NavLink>
-            <NavLink to="/aprendizaje">📚 Aprendizaje</NavLink>
-
-            <div className="sidebar-footer">
-                <p>Versión Local</p>
-                <span>Tus datos se guardan localmente en tu navegador</span>
-            </div>
-        </div>
+        </>
     );
 }
 
