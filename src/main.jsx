0323961df6c16885c4a333713231934009e15d03
Steven Alvarez <stevenalvarez699@gmail.com>
2026-06-21 23:12:18 -0500

    fix: corregir mayúscula en carpeta Context para despliegue en Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './responsive.css'
 import App from './App.jsx'
-import { ToastProvider } from './context/ToastContext';
+import { ToastProvider } from "./Context/ToastContext";
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
